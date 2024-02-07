450acec1ec0cf3eb96283dc186065bc05019eba6
itzJunaedRayhan <webgenesis99@gmail.com>
2024-02-07 23:12:06 +0600

    [DONE] Some Changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
 const cors = require('cors');
 
 app.use(cors());
